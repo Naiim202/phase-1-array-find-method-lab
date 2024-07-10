4c12e096d00ee2baf39d56c15be743b2ad877fe6
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-07-10 15:56:41 +0300

    array-find-method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+
+function superbowlWin(record) {
+    for (let entry of record) {
+      if (entry.result === 'W') {
+
+        return entry.year;
+      }
+    }
+    return undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
